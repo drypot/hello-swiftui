5600a4b67ab5aa8b61cb2b4aa7090c7e2b77b899
drypot <drypot@gmail.com>
2022-10-05 21:54:42 +0900

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by drypot on 2022/10/05.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
